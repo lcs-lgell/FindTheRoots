9cc581422c4605831bf9801a53b433c0d5260848
Leon Gell <lgell@lcs.on.ca>
2023-01-24 08:45:29 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Leon Gell on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
